8331174499f7f80b235029799279d108729133f8
Tongzhou-Yu <tongzhou.yu@network.rca.ac.uk>
2020-11-18 18:00:02 +0800

    Webcam Video Matting By Anycolor

---
+++ webcam realtime matting/Assets/Scripts/Cam.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cam : MonoBehaviour
+{
+	public string Mydevice;
+	WebCamTexture tex;
+
+	// Use this for initialization
+	IEnumerator Start()
+	{
+		yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+		if (Application.HasUserAuthorization(UserAuthorization.WebCam))
+		{
+			WebCamDevice[] devices = WebCamTexture.devices;
+			Mydevice = devices[0].name;
+			tex = new WebCamTexture(Mydevice, 400, 300, 12);
+			this.GetComponent<Renderer>().material.mainTexture = tex;
+			tex.Play();
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+
+	}
+
+}
